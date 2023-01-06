1b19bb2ad104e629cf7b5197715140e202b4f8a2
Priyanka <121668603+Mogavee@users.noreply.github.com>
2023-01-06 17:10:54 +0530

    Assingment 9

---
+++ Dotnet Assingment/Assingment_9_ReverseString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleConApp
+{
+
+    class ReverseString
+    {
+        public static void Reverse()
+        {
+            String name = Utilities.Prompt("Enter the sentence");
+            string[] temp = name.Split(' ');
+            String store = "";
+            for(int i=temp.Length-1;i>=0;i--)
+            {
+                store = string.Concat(store, temp[i]," ");
+            }
+            Console.WriteLine(store);
+        }
+        static void Main(string[] args)
+        {
+            Reverse();
+        }
+    }
+}
